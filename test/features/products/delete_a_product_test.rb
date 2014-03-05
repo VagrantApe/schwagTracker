299718e244e79e6a084973ae9d47b694336a6fb2
Ramin <rsedighi@gmail.com>
2014-03-05 11:29:53 -0800

    Fixed delete order test and added delete product test

---
+++ test/features/products/delete_a_product_test.rb
@@ -0,0 +1,13 @@
+require "test_helper"
+
+feature "As a user I want to visit the product page" do
+  scenario "to remove a product from product index" do
+
+    visit product_path products(:pr)
+
+    delete_product
+
+  end
+end
+
+
