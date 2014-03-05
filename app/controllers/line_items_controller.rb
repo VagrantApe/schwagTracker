719f058f9f26dce4ee0dca983dff73f8cd16a361
John Johnson <luckstedcraig@gmail.com>
2014-03-04 17:39:59 -0800

    js working but ugly

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,6 +1,6 @@
 class LineItemsController < ApplicationController
   before_action :set_line_item, only: [:show, :edit, :update, :destroy]
-  before_action :set_order, only: [:create, :edit, :destroy]
+  before_action :set_order, only: [:new, :update, :create, :edit, :destroy]
 
   def index
     @order
@@ -10,18 +10,18 @@ class LineItemsController < ApplicationController
   end
 
   def new
+    @line_item = LineItem.new
   end
 
   def edit
   end
 
   def create
-    @line_item = @order.line_items.new
+    @line_item = @order.line_items.new(line_item_params)
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item, notice: 'Line item was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @line_item }
+        format.html { redirect_to @order, notice: 'Line item was successfully created.' }
         format.js
       else
         format.html { render action: 'new' }
@@ -34,7 +34,6 @@ class LineItemsController < ApplicationController
     respond_to do |format|
       if @line_item.update(line_item_params)
         format.html { redirect_to @order, notice: 'Line item was successfully updated.' }
-        format.json { head :no_content }
         format.js
       else
         format.html { render action: 'edit' }
@@ -47,7 +46,6 @@ class LineItemsController < ApplicationController
     @line_item.destroy
     respond_to do |format|
       format.html { redirect_to line_items_url }
-      format.json { head :no_content }
       format.js
     end
   end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def shallow_args(parent, child)
+    params[:action] == 'new' ? parent : [parent, child]
+  end
 end
