c39db67090373ce7524804395da1eafd22e26a69
Andrew Thul <thul.andy@gmail.com>
2014-03-06 14:50:59 -0800

    complete awesome tables

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,7 +3,7 @@ class Product < ActiveRecord::Base
   has_many :orders, through: :line_items
   validates :name, presence: true
   validates :sku, uniqueness: true
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "images/:style/missing.png"
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "50x50>" }, :default_url => "images/:style/missing.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
 
