23934c24d5af233772380ccf1faaac597cc1589d
John Johnson <luckstedcraig@gmail.com>
2014-03-06 19:20:00 -0800

    seed admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-# admin_user = User.create!(:email => "admin@admin.com")
-# admin_password = User.create!(:password => "password")
-# admin_user.roles << admin_role
+admin_user = User.create!(email: "admin@admin.com", password: 'password', password_confirmation: 'password', role: 'admin')
+
