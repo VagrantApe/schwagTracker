25e81ecf04098ad0ff9ae0235cfebfe8eb8e92d6
John Johnson <luckstedcraig@gmail.com>
2014-03-06 16:00:56 -0800

    user sign up working

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 SchwagTracker::Application.routes.draw do
-  devise_for :users
+  devise_for :users, skip: :registrations
+  as :user do
+    get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
+    put 'users' => 'devise/registrations#update', :as => 'user_registration'
+  end
   resources :users
-
   root 'welcome#index'
 
   resources :products
